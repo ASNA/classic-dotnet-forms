18c630334b1004127ab961d3ffdd168fa4466fc3
Roger Pence <roger.pence@gmail.com>
2018-08-30 15:47:47 -0500

    update pycco comments

--- test.sh
+++ test.sh
@@ -1,13 +1,15 @@
 # 
 #
-dotnetfiles=(formMain.vr)
-classicfiles=(startup.vrp formMain.vrf formModalCall.vrf formModalExFmt.vrf formNonModalShow.vrf)
+#dotnetfiles=(formMain.vr)
+#classicfiles=(startup.vrp formMain.vrf formModalCall.vrf formModalExFmt.vrf formNonModalShow.vrf)
+
 
 cd .\\dotnet-project\\dotnet-project
 
 # Annotate classic project files.
 cd ..\\..\\classic-project
 pwd
+classicfiles=( $( ls *.{vrp,vrf} ) ) 
 for f in ${classicfiles[@]}; 
 do
 	echo "Filename: $f"
